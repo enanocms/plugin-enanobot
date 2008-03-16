a64b248e7c921b0757d50c1aa7898dfffcf5addc
Dan Fuhry <dan@enanocms.org>
2008-03-15 20:15:16 -0400

    Added support for logging mode messages and join/part messages

--- enanobot.php
+++ enanobot.php
@@ -48,6 +48,7 @@ function enanobot_channel_event_enano($sockdata, $chan)
   global $irc, $nick, $mysql_conn, $privileged_list;
   $sockdata = trim($sockdata);
   $message = Request_IRC::parse_message($sockdata);
+  enanobot_log_message($chan, $message);
   switch ( $message['action'] )
   {
     case 'JOIN':
@@ -68,6 +69,7 @@ function enanobot_channel_event_enanodev($sockdata, $chan)
   global $irc, $privileged_list;
   $sockdata = trim($sockdata);
   $message = Request_IRC::parse_message($sockdata);
+  enanobot_log_message($chan, $message);
   switch ( $message['action'] )
   {
     case 'JOIN':
@@ -84,32 +86,6 @@ function enanobot_channel_event_enanodev($sockdata, $chan)
 function enanobot_process_channel_message($sockdata, $chan, $message)
 {
   global $irc, $nick, $mysql_conn, $privileged_list;
-  
-  // Log the message
-  $chan_db = mysql_real_escape_string($chan->get_channel_name());
-  $nick_db = mysql_real_escape_string($message['nick']);
-  $line_db = mysql_real_escape_string($message['message']);
-  $day     = date('Y-m-d');
-  $time    = time();
-  $m_et = false;
-  while ( true )
-  {
-    $q = @mysql_query("INSERT INTO irclog(channel, day, nick, timestamp, line) VALUES
-                         ( '$chan_db', '$day', '$nick_db', '$time', '$line_db' );");
-    if ( !$q )
-    {
-      $m_e = mysql_error();
-      $m_et = true;
-      if ( $m_e == 'MySQL server has gone away' && !$m_et )
-      {
-        mysql_reconnect();
-        continue;
-      }
-      $irc->close("MySQL query error: $m_e");
-      exit(1);
-    }
-    break;
-  }
   
   if ( preg_match('/^\!echo /', $message['message']) && in_array($message['nick'], $privileged_list) )
   {
@@ -162,6 +138,60 @@ function enanobot_process_channel_message($sockdata, $chan, $message)
   {
     $target_nick =& $message['nick'];
     $chan->msg("{$target_nick}, I'm only a bot. :-) You should probably rely on the advice of humans if you need further assistance.", true);
+  }
+}
+
+function enanobot_log_message($chan, $message)
+{
+  global $nick;
+  
+  // Log the message
+  $chan_db = mysql_real_escape_string($chan->get_channel_name());
+  $nick_db = mysql_real_escape_string($message['nick']);
+  $line_db = mysql_real_escape_string($message['message']);
+  $day     = date('Y-m-d');
+  $time    = time();
+  $m_et = false;
+  $sql = false;
+  switch($message['action'])
+  {
+    case 'PRIVMSG':
+      $sql = "INSERT INTO irclog(channel, day, nick, timestamp, line) VALUES
+                ( '$chan_db', '$day', '$nick_db', '$time', '$line_db' );";
+      break;
+    case 'JOIN':
+      $sql = "INSERT INTO irclog(channel, day, nick, timestamp, line) VALUES
+                ( '$chan_db', '$day', '', '$time', '$nick_db has joined $chan_db' );";
+      break;
+    case 'PART':
+      $sql = "INSERT INTO irclog(channel, day, nick, timestamp, line) VALUES
+                ( '$chan_db', '$day', '', '$time', '$nick_db has left $chan_db' );";
+      break;
+    case 'MODE':
+      list($mode, $target_nick) = explode(' ', $line_db);
+      if ( $message['nick'] != 'ChanServ' && $target_nick != $nick )
+      {
+        $sql = "INSERT INTO irclog(channel, day, nick, timestamp, line) VALUES
+                  ( '$chan_db', '$day', '', '$time', '$nick_db set mode $mode on $target_nick' );";
+      }
+      break;
+  }
+  while ( true && $sql )
+  {
+    $q = @mysql_query($sql);
+    if ( !$q )
+    {
+      $m_e = mysql_error();
+      $m_et = true;
+      if ( $m_e == 'MySQL server has gone away' && !$m_et )
+      {
+        mysql_reconnect();
+        continue;
+      }
+      $irc->close("MySQL query error: $m_e");
+      exit(1);
+    }
+    break;
   }
 }
 
